7da764a78bc8d31d5ed785fee72387e4957fea24
arommers <arommers@codam.student.nl>
2024-04-29 10:19:13 +0200

    wp not installed yet

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+cd /var/lib/mysql
+
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
 	if [ ! -f db.sql ]; then
 		echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > db.sql
 		echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db.sql
+		echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';" >> db.sql
 		echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db.sql
 		echo "FLUSH PRIVILEGES;" >> db.sql
 	fi
+
+	mariadb-install-db \
+	--user=mysql \
+	--datadir=/var/lib/mysql \
+	--skip-test-db \
+	--skip-name-resolve \
+	--auth-root-authentication-method=normal
+
+	sleep 2
 
 	service mariadb start
 	mysql < db.sql
--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -10,17 +10,20 @@ if [ ! -f ./wp-config.php ]; then
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
+	sleep 2
+
 	sed -i "s/username_here/$DB_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$DB_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$DB_HOST/g" wp-config-sample.php
 	sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 
-#	wp user create --allow-root $WP_ADMIN $WP_ADMIN_EMAIL \
-#	--role=administrator user_pass=$WP_ADMIN_PASS
+	wp core install --allow-root -url="$DOMAIN_NAME" --title="$WP_TITLE" \
+	--admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASS" \
+	--admin_email="WP_ADMIN_EMAIL" --skip-email
 
-#	wp user create --allow-root $WP_USER $WP_USER_EMAIL \
-#	--role=author user_pass=$WP_PASSWORD
+	wp user create --allow-root "$WP_USER" "$WP_USER_EMAIL" \
+	--role=author user_pass="$WP_PASSWORD" --skip-email
 
 fi
 
